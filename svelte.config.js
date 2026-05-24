9d2b71cae47df4df42109e13c5c6df6979846448
Ryan Atkinson <mail@ryanatkn.com>
2026-05-24 15:06:06 -0400

    format

--- svelte.config.js
+++ svelte.config.js
@@ -13,9 +13,7 @@ export default {
 		adapter: adapter(),
 		paths: {relative: false}, // use root-absolute paths for SSR path comparison: https://svelte.dev/docs/kit/configuration#paths
 		alias: {$routes: 'src/routes'},
-		version: {
-			name: execSync('git rev-parse HEAD').toString().trim(),
-		},
+		version: {name: execSync('git rev-parse HEAD').toString().trim()},
 
 		// Example CSP using https://ui.fuz.dev/docs/csp
 		//
